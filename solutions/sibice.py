824882f5a7e82c625039fed38991324c5c2419ae
tpercy <tpercy@linus.highpoint.edu>
2024-06-20 09:18:49 -0400

    sibice

--- solutions/sibice.py
+++ solutions/sibice.py
@@ -5,12 +5,10 @@ n, w, h = [int(x) for x in input().split()]
 ans = []
 for _ in range(0, n):
     x = int(input())
-    if w >= x or h >= x or sqrt(x) <= sqrt((pow(w, 2) + pow(h, 2))):
+    if w >= x or h >= x or (x <= sqrt((pow(w, 2) + pow(h, 2)))):
         ans.append("DA")
     else:
         ans.append("NE")
 
 for a in ans:
     print(a)
-
-# fix a^2 + b^2 = c^2
